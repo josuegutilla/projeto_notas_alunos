b10569ed8c93a2630d9609c5a161740fb9df2230
Josué <gutillaj@gmail.com>
2023-01-20 16:10:01 -0400

    projeto notas aluno

---
+++ Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_Nota_Aluno
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+        public double SomaNota()
+        {
+            double Notas = (Nota1 + Nota2 + Nota3) / 3;
+
+            return Notas;
+        }
+
+        public double FaltaNota()
+        {
+            double Restante = 6 - SomaNota();
+
+            return Restante;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ex_Nota_Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno(); //criando a variavel da classe e instanciando.
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno (1 a 10): ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine()); 
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Nota final = " + aluno.SomaNota().ToString("F1", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            if(aluno.SomaNota() > 6) 
+            {
+                Console.WriteLine("ALUNO APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("ALUNO REPROVADO");
+                Console.WriteLine("FALTARAM " + aluno.FaltaNota().ToString("F1", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+    }
+}
